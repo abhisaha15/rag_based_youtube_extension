3a36e04b4a1e8d9fe8fd4724489d2c0f15de1c49
Abhishek Saha <abhisheksaha2018@gmail.com>
2026-03-28 03:37:02 +0530

    Update rag_service.py
    Depreciated modules

--- services/rag_service.py
+++ services/rag_service.py
@@ -1,5 +1,5 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
 
